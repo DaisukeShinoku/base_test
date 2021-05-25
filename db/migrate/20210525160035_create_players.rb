097ddf42041ae7c62feb2168bd4991a00f4dec90
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2021-05-26 01:32:43 +0900

    Userテーブルの作成

---
+++ db/migrate/20210525160035_create_players.rb
@@ -0,0 +1,15 @@
+class CreatePlayers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :players, comment: 'プレイヤー' do |t|
+      t.string :account_name, null: false, limit: 15, comment: 'アカウント名'
+      t.integer :prefecture_code, comment: '都道府県コード'
+      t.string :email, null: false, comment: 'メールアドレス'
+      t.string :password_digest, null: false, comment: 'パスワード'
+      t.text :profile, comment: 'プロフィール文'
+      t.string :image, comment: 'プロフィール画像'
+
+      t.timestamps
+    end
+    add_index :players, :email, unique: true
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,30 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_05_25_160035) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "players", comment: "プレイヤー", force: :cascade do |t|
+    t.string "account_name", limit: 15, null: false, comment: "アカウント名"
+    t.integer "prefecture_code", comment: "都道府県コード"
+    t.string "email", null: false, comment: "メールアドレス"
+    t.string "password_digest", null: false, comment: "パスワード"
+    t.text "profile", comment: "プロフィール文"
+    t.string "image", comment: "プロフィール画像"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_players_on_email", unique: true
+  end
+
+end
